29fb82101edc261e8c7850f0f110741d77f97d83
Rafaele Wylde <raffaelewylde@gmail.com>
2025-04-27 09:55:59 -0400

    small updates

--- myfilter.py
+++ myfilter.py
@@ -2,12 +2,14 @@ import fitz
 import re
 
 
-def toc_existence_check(pdf_file):
+def toc_chapter_1_check(pdf_file):
     doc = fitz.open(pdf_file)
-    pattern = r"\bchapter 1(?!\d)"
+    pattern1 = r"\bchapter 1(?!\d)"
+    pattern2 = r"\b(1|one)[-:\.,]"
     toc = doc.get_toc()
     if toc:
         for item in toc:
-            match = re.search(pattern, item[1], re.IGNORECASE)
-            if match:
+            match1 = re.search(pattern1, item[1], re.IGNORECASE)
+            match2 = re.search(pattern2, item[1], re.IGNORECASE)
+            if match1 or match2:
                 return item[2]
